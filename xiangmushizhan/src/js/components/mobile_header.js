a36b864f5d46732c8ef2a17696028a3b906d5516
wansu <wansu@segasoft.cn>
2017-09-08 15:58:32 +0800

    init

--- xiangmushizhan/src/js/components/mobile_header.js
+++ xiangmushizhan/src/js/components/mobile_header.js
@@ -1,20 +1,13 @@
 import React from 'react';
-
 export default class MobileHeader extends React.Component {
-    render() {
-        return (
-            <div id="mobileheader">
-                <header>
-                    <img src="./src/images/logo.png" alt="logo"/>
-                    <span>React News</span>
-
-                </header>
-
-            </div>
-
-
-        );
-
-    };
-
+	render() {
+		return (
+      <div id="mobileheader">
+        <header>
+          <img src="./src/images/logo.png" alt="logo"/>
+          <span>ReactNews</span>
+        </header>
+      </div>
+		);
+	};
 }
